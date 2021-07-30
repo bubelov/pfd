435b7cd73d721cd2d282303b6445c0358fc20ee9
Igor Bubelov <igor@bubelov.com>
2021-07-30 20:27:58 +0700

    Create separate in-memory database for each test

--- src/tests.rs
+++ src/tests.rs
@@ -1,28 +1,20 @@
-use crate::{
-    model::ExchangeRate,
-    prepare,
-    repository::exchange_rates,
-    {db, db::DbVersion},
-};
+use crate::{db, model::ExchangeRate, prepare, repository::exchange_rates};
 use rocket::{http::Status, local::blocking::Client};
 use rusqlite::Connection;
-use serial_test::serial;
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+static COUNTER: AtomicUsize = AtomicUsize::new(1);
 
 fn setup() -> (Client, Connection) {
-    let conf = rocket::Config::figment().select("test");
+    let db_name = COUNTER.fetch_add(1, Ordering::Relaxed);
+    let db_url = format!("file::testdb_{}:?mode=memory&cache=shared", db_name);
+    let conf = rocket::Config::figment().merge(("databases.main.url", db_url));
     let rocket = prepare(rocket::custom(&conf));
     let client = Client::untracked(rocket).unwrap();
     (client, db::connect(&conf))
 }
 
-fn drop_db() {
-    let conf = rocket::Config::figment().select("test");
-    let mut conn = db::connect(&conf);
-    db::migrate(&conf, &mut conn, DbVersion::Specific(0));
-}
-
 #[test]
-#[serial]
 fn exchange_rates_controller_get() {
     let (client, mut db) = setup();
 
@@ -39,15 +31,11 @@ fn exchange_rates_controller_get() {
     assert_eq!(res.status(), Status::Ok);
     let body = res.into_json::<ExchangeRate>().unwrap();
     assert_eq!(rate, body);
-
-    drop_db();
 }
 
 #[test]
-#[serial]
 fn exchange_rates_controller_get_should_return_404_if_not_found() {
     let (client, _) = setup();
     let res = client.get("/exchange_rates?base=USD&quote=EUR").dispatch();
     assert_eq!(res.status(), Status::NotFound);
-    drop_db();
 }
