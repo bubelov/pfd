7918c23844257c10294f71b26eab3cfb2be09056
Igor Bubelov <igor@bubelov.com>
2021-08-01 21:13:59 +0700

    Simplify exchange rates controller

--- src/controller/exchange_rates.rs
+++ src/controller/exchange_rates.rs
@@ -14,41 +14,45 @@ pub async fn get(base: &str, quote: &str, db: Db) -> Result<Json<ExchangeRate>, 
         .run(move |conn| exchange_rates::select_by_base_and_quote(conn, &base_owned, &quote_owned))
         .await;
 
-    match rate {
-        Ok(rate) => match rate {
-            Some(rate) => Ok(Json(rate)),
-            None => {
-                let base_owned = base.to_string();
-                let quote_owned = quote.to_string();
-
-                let inversed_rate = db
-                    .run(move |conn| {
-                        exchange_rates::select_by_base_and_quote(conn, &quote_owned, &base_owned)
-                    })
-                    .await;
-
-                match inversed_rate {
-                    Ok(rate) => match rate {
-                        Some(rate) => Ok(Json(ExchangeRate {
-                            base: base.to_string(),
-                            quote: quote.to_string(),
-                            rate: 1.0 / rate.rate,
-                        })),
-                        None => Err(Error {
-                            code: 404,
-                            message: "Not found".to_string(),
-                        }),
-                    },
-                    Err(_e) => Err(Error {
-                        code: 500,
-                        message: "Internal server error".to_string(),
-                    }),
-                }
-            }
-        },
-        Err(_e) => Err(Error {
+    if rate.is_err() {
+        return Err(Error {
             code: 500,
             message: "Internal server error".to_string(),
-        }),
+        });
     }
+
+    let rate = rate.unwrap();
+
+    if rate.is_some() {
+        return Ok(Json(rate.unwrap()));
+    }
+
+    let base_owned = base.to_string();
+    let quote_owned = quote.to_string();
+
+    let rate = db
+        .run(move |conn| exchange_rates::select_by_base_and_quote(conn, &quote_owned, &base_owned))
+        .await;
+
+    if rate.is_err() {
+        return Err(Error {
+            code: 500,
+            message: "Internal server error".to_string(),
+        });
+    }
+
+    let rate = rate.unwrap();
+
+    if rate.is_some() {
+        return Ok(Json(ExchangeRate {
+            base: base.to_string(),
+            quote: quote.to_string(),
+            rate: 1.0 / rate.unwrap().rate,
+        }));
+    }
+
+    Err(Error {
+        code: 404,
+        message: "Not found".to_string(),
+    })
 }
