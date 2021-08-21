8c77952ef707555c11f2f28981d38a62ac7994af
Igor Bubelov <igor@bubelov.com>
2021-08-22 02:06:27 +0700

    Simplify tests

--- src/test.rs
+++ src/test.rs
@@ -10,12 +10,14 @@ use std::sync::atomic::{AtomicUsize, Ordering};
 
 static COUNTER: AtomicUsize = AtomicUsize::new(1);
 
-static AUTH_TOKEN: &str = "5110afcc-f3cc-420e-bb8c-a4f425af74c8";
-
 pub fn client() -> Client {
+    const AUTH_TOKEN: &str = "5110afcc-f3cc-420e-bb8c-a4f425af74c8";
+
     let db_name = COUNTER.fetch_add(1, Ordering::Relaxed);
     let db_url = format!("file::testdb_{}:?mode=memory&cache=shared", db_name);
+
     let conf = rocket::Config::figment().merge(("databases.main.url", &db_url));
+
     let rocket = prepare(rocket::custom(&conf)).attach(AdHoc::on_request("Authorize", |req, _| {
         Box::pin(async move {
             req.add_header(Header::new(
@@ -24,21 +26,27 @@ pub fn client() -> Client {
             ));
         })
     }));
+
     let client = Client::untracked(rocket).unwrap();
+
     let user = User {
         username: "test".to_string(),
         password_hash:
             "$argon2i$v=19$m=4096,t=3,p=1$dGVzdHNhbHQ$vZDbLe7RwrtwcAi3fktiLHdK3/PLogGcGuCgDpoINUc"
                 .to_string(),
     };
-    let token = AuthToken {
-        id: AUTH_TOKEN.parse().unwrap(),
-        username: user.username.clone(),
-    };
+
     let user_repo = client.rocket().state::<UserRepository>().unwrap();
     user_repo.insert(&user).unwrap();
+
     let token_repo = client.rocket().state::<AuthTokenRepository>().unwrap();
-    token_repo.insert(&token).unwrap();
+    token_repo
+        .insert(&AuthToken {
+            id: AUTH_TOKEN.parse().unwrap(),
+            username: user.username.clone(),
+        })
+        .unwrap();
+
     client
 }
 
